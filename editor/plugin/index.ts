c25da009ecaf7bfa99bcbbbbb1355f8a70a71f78
HD <zhd.superman@gmail.com>
2022-08-02 23:56:12 +0800

    feat: add input rule plugin

--- editor/plugin/index.ts
+++ editor/plugin/index.ts
@@ -3,11 +3,13 @@ import { Plugin } from 'prosemirror-state';
 import { keymap } from 'prosemirror-keymap';
 import { baseKeymap } from 'prosemirror-commands';
 import blockIndex from './block-index';
+import buildInputRules from './input-rules';
 import bindKeymap from './keymap';
 import schema from '../schema';
 
 const plugins: Plugin[] = [
   blockIndex(),
+  buildInputRules(schema),
   history(),
   keymap(bindKeymap(schema)),
   keymap(baseKeymap),
