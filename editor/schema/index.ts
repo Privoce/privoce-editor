b082bfa9080af106e6002fa9c8c4f41d15ad0761
HD <zhd.superman@gmail.com>
2022-08-02 23:49:56 +0800

    feat: add code block in editor

--- editor/schema/index.ts
+++ editor/schema/index.ts
@@ -49,6 +49,31 @@ const baseSchema = new Schema({
         return [`h${node.attrs.level}`, { bid, hid }, 0];
       },
     },
+    code_block: {
+      attrs: { info: { default: '' } },
+      group: 'block',
+      marks: '',
+      content: 'text*',
+      code: true,
+      defining: true,
+      parseDOM: [
+        {
+          tag: 'pre',
+          preserveWhitespace: 'full',
+          getAttrs(node) {
+            if (typeof node === 'string') return { info: '' };
+            const el = node as HTMLElement;
+            let info: string | null;
+            info = el.getAttribute('info');
+            if (info) return { info };
+            info = el.getAttribute('lang');
+            if (info) return { info };
+            return { info: '' };
+          },
+        },
+      ],
+      toDOM: node => ['pre', { info: node.attrs.info }, ['code', 0]],
+    },
     text: {
       inline: true,
       group: 'inline',
